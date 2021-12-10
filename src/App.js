d433f0bdb39b5129663f2a18b5aefb8c675a3694
Laleet <laleetpuree5768@gmail.com>
2021-12-10 19:14:03 +0530

    added initial files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import NavBar from "./components/NavBar";
+import Headings from "./components/Headings";
+import AttractiveContainer from "./components/AttractiveContainer";
+import Featured from "./components/Featured";
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <NavBar />
+
+        <Headings />
+        <AttractiveContainer />
+        <Featured />
+        <Post />
+      </div>
+    </>
+  );
+}
+
+export default App;
